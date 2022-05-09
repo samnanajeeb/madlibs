912865981f4d077fdc93f8b8fbbd55d80f9aab32
Samna Najeeb <66362347+samnanajeeb@users.noreply.github.com>
2022-05-09 15:17:54 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,26 @@
+""" String concatenation means how to put strings together
+"""
+"""youtuber = "samna"
+print("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
+
+noun1 = input("Noun: ")
+verb1 = input("verb1: ")
+noun2 = input("noun2: ")
+noun3 = input("nount3: ")
+madlib = f"Twinkle, Twinkle littel {noun1}, How I {verb1} what you are.\
+Up above the {noun2} so high. line a {noun2} in the {noun3}"
+print (madlib)"""
+
+def madlib():
+       noun1 = input("Noun: ")
+       verb1 = input("verb1: ")
+       noun2 = input("noun2: ")
+       noun3 = input("nount3: ")
+       madlib = f"Twinkle, Twinkle littel {noun1}, How I {verb1} what you are.\
+Up above the {noun2} so high. line a {noun2} in the {noun3}"
+       print(madlib)
+
+if __name__ == '__main__':
+        madlib()
